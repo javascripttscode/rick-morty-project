0452446d1ec11e35294c343d53ed3be77c615972
CzrLts <cezarltsv@gmail.com>
2023-12-18 17:29:49 -0300

    feat: create footer component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CardCharacterComponent } from '../../shared/components/card-character/card-character.component';
+import { DefaultFooterComponent } from '../../shared/components/footer/default-footer.component';
 
 @Component({
     selector: 'home',
     standalone: true,
-    imports: [CardCharacterComponent],
+    imports: [CardCharacterComponent, DefaultFooterComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
---
+++ src/app/shared/components/footer/default-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'default-footer',
+    standalone: true,
+    templateUrl: './default-footer.component.html',
+    styleUrls: ['./default-footer.component.scss'],
+})
+export class DefaultFooterComponent {}
